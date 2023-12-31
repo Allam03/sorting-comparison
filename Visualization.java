d808d8b5aec979e304aa3c79d98b616aaa3a4c5e
Allam <mohamed62hesham@gmail.com>
2023-12-31 14:49:20 +0200

    Added Y axis to barplot

--- Visualization.java
+++ Visualization.java
@@ -18,25 +18,32 @@ public class Visualization extends JPanel {
     private void drawBarPlot(Graphics g) {
         if (data == null || data.length == 0)
             return;
-
+    
         int width = getWidth();
         int height = getHeight();
         int barWidth = (width / data.length);
-
         int maxData = Arrays.stream(data).max().orElse(0);
-
+    
+        g.drawLine(30, 10, 30, height - 10);
+    
+        int step = maxData / 10;
+    
+        for (int i = 0; i <= maxData; i += step) {
+            int labelY = height - ((i * (height - 20)) / maxData);
+            g.drawString(String.valueOf(i), 5, labelY);
+            g.drawLine(30, labelY, 35, labelY);
+        }
+    
         for (int i = 0; i < data.length; i++) {
-            int barHeight = (int) (((double) data[i] / maxData) * (height - 100));
-            int x = i * barWidth + 25;
-            int y = height - barHeight - 25;
-
+            int barHeight = (int) (((double) data[i] / maxData) * (height - 50));
+            int x = (i * barWidth) + 30;
+            int y = height - barHeight - 10;
+    
             g.setColor(Color.BLUE);
             g.fillRect(x, y, barWidth, barHeight);
-
+    
             g.setColor(Color.BLACK);
             g.drawRect(x, y, barWidth, barHeight);
-
-            g.drawString(String.valueOf(data[i]), x, y - 10);
         }
     }
 
